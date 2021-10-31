c5d4444cc97eff9e3924f2c05e71f44a9f32df1a
pratik singh <1809193@sbsstc.ac.in>
2021-10-31 20:58:44 +0530

    #7

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-qblmun7w@++*kt4yp8*-h!^r-7*0on$szf9y^io@@ski)&mz=h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['pratiksingh.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
